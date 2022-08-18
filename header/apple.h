dc48d126b24d3f12133da0ba1a173f62441b0e8f
Leluan0109 <levanluan0109@gmail.com>
2022-08-18 09:55:13 +0700

    header

---
+++ header/apple.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Apple {
+private:
+    RectangleShape sprite;
+
+public:
+    Apple();
+
+    void setPosition(Vector2f newPosition);
+    RectangleShape getSprite();
+};
+
